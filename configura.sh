f1f5511b2dec3345186166bcd319b27c684037d4
dexdevis <132813755+dexdevis@users.noreply.github.com>
2024-08-29 18:10:30 +0200

    Add files via upload

---
+++ configura.sh
@@ -0,0 +1,306 @@
+#!/usr/bin/bash
+
+################################################
+##### Orologio
+################################################
+
+# Enable systemd-timesyncd
+systemctl enable systemd-timesyncd.service
+# -------------------------------------------> verificare file di configurazione
+
+
+# Set timezone
+ln -sf /usr/share/zoneinfo/Europe/Rome /etc/localtime
+hwclock --systohc
+
+################################################
+##### Impostazione della lingua
+################################################
+
+# Locale
+echo "it_IT.UTF-8 UTF-8" > /etc/locale.gen
+echo "LANG=\"it_IT.UTF-8\"" > /etc/locale.conf
+locale-gen
+
+# Keymap
+echo "KEYMAP=it" > /etc/vconsole.conf
+
+################################################
+##### Hostname
+################################################
+
+# Set hostname
+echo ${NEW_HOSTNAME} > /etc/hostname
+
+# Set /etc/hosts
+tee /etc/hosts << EOF
+127.0.0.1 localhost
+::1 localhost
+127.0.1.1 ${NEW_HOSTNAME}.localdomain ${NEW_HOSTNAME}
+EOF
+
+################################################
+##### Pacman
+################################################
+
+# Abilita repository multilib
+sed -i '/#\[multilib\]/{N;s/#\[multilib\]\n#Include = \/etc\/pacman.d\/mirrorlist/\[multilib\]\nInclude = \/etc\/pacman.d\/mirrorlist/}' /etc/pacman.conf
+pacman -Syy
+
+# Inizializza il keyring di pacman
+pacman-key --init
+pacman-key --populate
+
+# Configura Pacman
+sed -i "s|^#Color|Color|g" /etc/pacman.conf
+sed -i "s|^#VerbosePkgLists|VerbosePkgLists|g" /etc/pacman.conf
+sed -i "s|^#ParallelDownloads.*|ParallelDownloads = 5|g" /etc/pacman.conf
+
+# Aggiorna il sistema
+pacman -Syu
+
+################################################
+##### Installa applicazioni di base
+################################################
+
+pacman -S --noconfirm \
+    grub \
+    sudo \
+    bash-completion \
+    dialog \    
+    wpa_supplicant \    
+    mtools \
+    dosfstools \    
+    coreutils \
+    util-linux \
+    inetutils \
+    xdg-utils \
+    xdg-user-dirs \
+    alsa-utils \    
+    htop \
+    git \
+    p7zip \
+    unzip \
+    unrar \
+    which \
+    man-db \
+    man-pages \
+    rsync \
+    ufw \
+    zram-generator
+
+
+################################################
+##### zram (swap)
+################################################
+
+# Configura zram generator
+tee /etc/systemd/zram-generator.conf << EOF
+[zram0]
+zram-size = ram / 4
+compression-algorithm = zstd
+EOF
+
+systemctl daemon-reload
+systemctl start /dev/zram0
+echo 'vm.page-cluster=0' > /etc/sysctl.d/99-page-cluster.conf
+echo 'vm.swappiness=10' > /etc/sysctl.d/99-swappiness.conf
+echo 'vm.vfs_cache_pressure=50' > /etc/sysctl.d/99-vfs-cache-pressure.conf
+
+
+################################################
+##### Utenti
+################################################
+
+# Setta la password di root
+echo "root:${NEW_USER_PASSWORD}" | passwd
+
+# Setup nuovo utente
+useradd -m -G wheel ${NEW_USER}
+echo "${NEW_USER}:${NEW_USER_PASSWORD}" | passwd
+echo "%wheel ALL=(ALL) ALL" >> /etc/sudoers
+
+# Crea le cartelle utente
+sudo -u ${NEW_USER} LC_ALL=it_IT.UTF-8 xdg-user-dirs-update --force
+
+
+################################################
+##### Firewall e rete
+################################################
+
+systemctl enable ufw.service
+ufw default allow outgoing
+ufw default deny incoming
+ufw logging off
+ufw enable
+
+# Chiedo in input i nomi delle interfacce di rete e i dati della rete wifi
+clear
+ip link
+read -p "Inserire il nome della scheda di rete Ethernet: " ETH
+read -p "Inserire il nome della scheda di rete Wifi: " WIFI # <--------------------------------------------------------
+
+clear
+read -p "Inserire il nome della rete Wifi: " ESSID # <--------------------------------------------------------
+read -sp "Inserire la password: " PASS # <--------------------------------------------------------
+
+# Creo il file di configurazione dell'interfaccia Wifi <-----------------------------------------------------------
+tee /etc/wpa_supplicant/wpa_supplicant-${WIFI}.conf << EOF
+ctrl_interface=/var/run/wpa_supplicant
+eapol_version=1
+ap_scan=1
+fast_reauth=1
+EOF
+
+# Creo la configurazione per la connessione Ethernet
+tee /etc/systemd/network/20-wired.network << EOF
+[Match]
+Name=${ETH}
+
+[Network]
+DHCP=yes
+DNS=1.1.1.1 1.0.0.1
+
+[DHCPv4]
+RouteMetric=100
+
+[IPv6AcceptRA]
+RouteMetric=100
+EOF
+
+# Creo la configurazione per la connessione Wireless <------------------------------------------------
+tee /etc/systemd/network/25-wireless.network << EOF
+[Match]
+Name=${WIFI}
+
+[Network]
+DHCP=yes
+IgnoreCarrierLoss=3s
+DNS=1.1.1.1 1.0.0.1
+
+[DHCPv4]
+RouteMetric=600
+
+[IPv6AcceptRA]
+RouteMetric=600
+EOF
+
+systemctl enable systemd-networkd.service
+systemctl enable systemd-resolved.service
+wpa_passphrase ${ESSID} '${PASS}' >> /etc/wpa_supplicant/wpa_supplicant-${WIFI}.conf
+
+# Cancella la password in chiaro
+sed -i "s|^${PASS}|********|g" /etc/wpa_supplicant/wpa_supplicant-${WIFI}.conf
+systemctl enable wpa_supplicant@${WIFI}.service
+
+
+################################################
+##### Initramfs
+################################################
+
+# Configura mkinitcpio
+sed -i "s|MODULES=()|MODULES=(btrfs${MKINITCPIO_MODULES})|" /etc/mkinitcpio.conf
+mkinitcpio -P
+
+################################################
+##### GRUB
+################################################
+
+grub-install --target=x86_64-efi --bootloader-id=GRUB --recheck /dev/nvme0n1 # <------------------------------------------------------
+grub-mkconfig -o /boot/grub/grub.cfg
+
+
+################################################
+##### GPU
+################################################
+
+# Installa i drivers della GPU
+pacman -S --noconfirm mesa vulkan-icd-loader vulkan-mesa-layers ${GPU_PACKAGES}
+
+# Override VA-API driver via environment variable
+tee -a /etc/environment << EOF
+
+# VA-API
+${LIBVA_ENV_VAR}
+EOF
+
+# If GPU is AMD, use RADV's Vulkan driver
+if lspci | grep "VGA" | grep "AMD" > /dev/null; then
+tee -a /etc/environment << EOF
+
+# Vulkan
+AMD_VULKAN_ICD=RADV
+EOF
+fi
+
+# Install VA-API tools
+pacman -S --noconfirm libva-utils
+
+# Install Vulkan tools
+pacman -S --noconfirm vulkan-tools
+
+################################################
+##### Systemd
+################################################
+
+# Configura il timeout di default per le unità di sistema
+mkdir -p /etc/systemd/system.conf.d
+tee /etc/systemd/system.conf.d/default-timeout.conf << EOF
+[Manager]
+DefaultTimeoutStopSec=5s
+EOF
+
+# Configura il timeout di default per le unità utente
+mkdir -p /etc/systemd/user.conf.d
+tee /etc/systemd/user.conf.d/default-timeout.conf << EOF
+[Manager]
+DefaultTimeoutStopSec=5s
+EOF
+
+################################################
+##### GStreamer
+################################################
+
+pacman -S --noconfirm \
+    gstreamer \
+    gst-libav \
+    gst-plugins-base \
+    gst-plugins-good \
+    gst-plugins-bad \
+    gst-plugins-ugly \
+    gstreamer-vaapi
+
+
+################################################
+##### Fonts
+################################################
+
+pacman -S --noconfirm \
+    noto-fonts \
+    noto-fonts-emoji \
+    noto-fonts-cjk \
+    noto-fonts-extra \
+    ttf-liberation \
+    otf-cascadia-code \
+    otf-commit-mono-nerd \
+    ttf-firacode-nerd \
+    ttf-hack-nerd \
+ 	ttf-noto-nerd \
+    ttf-sourcecodepro-nerd \
+    ttf-ubuntu-nerd \
+    ttf-ubuntu-mono-nerd \
+    ttf-hack \
+    inter-font \
+    cantarell-fonts \
+    otf-font-awesome
+
+################################################
+##### Cleanup
+################################################
+
+# Attribuire correttamente i permessi alla home del nuovo utente
+chown -R ${NEW_USER}:${NEW_USER} /home/${NEW_USER}
+
+# Esce da chroot
+exit
+
---
+++ installa.sh
@@ -0,0 +1,94 @@
+#!/usr/bin/bash
+
+clear
+
+read -p "Inserire un nuovo utente: " NEW_USER
+export NEW_USER
+
+read -sp "Password del nuovo utente: " NEW_USER_PASSWORD
+export NEW_USER_PASSWORD
+
+read -p "Nome Host del computer: " NEW_HOSTNAME
+export NEW_HOSTNAME
+
+# Determina la CPU
+if cat /proc/cpuinfo | grep "vendor" | grep "GenuineIntel" > /dev/null; then
+    export CPU_MICROCODE="intel-ucode"
+elif cat /proc/cpuinfo | grep "vendor" | grep "AuthenticAMD" > /dev/null; then
+    export CPU_MICROCODE="amd-ucode"
+    export AMD_SCALING_DRIVER="amd_pstate=active"
+fi
+
+# Determina la GPU
+if lspci | grep "VGA" | grep "Intel" > /dev/null; then
+    export GPU_PACKAGES="vulkan-intel intel-media-driver intel-gpu-tools"
+    export MKINITCPIO_MODULES=" i915"
+    export LIBVA_ENV_VAR="LIBVA_DRIVER_NAME=iHD"
+elif lspci | grep "VGA" | grep "AMD" > /dev/null; then
+    export GPU_PACKAGES="vulkan-radeon libva-mesa-driver radeontop"
+    export MKINITCPIO_MODULES=" amdgpu"
+    export LIBVA_ENV_VAR="LIBVA_DRIVER_NAME=radeonsi"
+fi
+
+################################################
+##### Partiziona il disco 
+################################################
+
+# Legge la tabella partizioni
+partprobe /dev/nvme0n1 # <----------------------------------
+
+# Cancella il disco
+wipefs -af /dev/nvme0n1
+sgdisk --zap-all --clear /dev/nvme0n1 # <----------------------------------
+partprobe /dev/nvme0n1 # <----------------------------------
+
+# Partiziona il disco
+sgdisk -n 1:0:+300M -t 1:ef00 /dev/nvme0n1 # <----------------------------------
+sgdisk -n 2:0:0 -t 2:8300 /dev/nvme0n1 # <----------------------------------
+partprobe /dev/nvme0n1 # <----------------------------------
+
+# Formatta le partizioni
+mkfs.btrfs /dev/nvme0n1p2 # <----------------------------------
+mkfs.fat -F32 /dev/nvme0n1p1 # <----------------------------------
+
+# Crea i sottovolumi
+mount /dev/nvme0n1p2 /mnt # <----------------------------------
+btrfs su cr /mnt/@
+btrfs su cr /mnt/@home
+umount /mnt
+
+# Monta i sottovolumi
+mount -o noatime,compress=zstd,subvol=@ /dev/nvme0n1p2 /mnt # <----------------------------------
+mkdir -p /mnt/home
+mkdir -p /mnt/boot/efi
+mount -o noatime,compress=zstd,subvol=@home /dev/nvme0n1p2 /mnt/home # <----------------------------------
+mount /dev/nvme0n1p1 /mnt/boot/efi # <----------------------------------
+
+
+################################################
+##### Installa il sistema base
+################################################
+
+# Importa la mirrorlist
+tee /etc/pacman.d/mirrorlist << 'EOF'
+Server = https://europe.mirror.pkgbuild.com/$repo/os/$arch
+Server = https://geo.mirror.pkgbuild.com/$repo/os/$arch
+Server = https://it.mirrors.cicku.me/archlinux/$repo/os/$arch
+EOF
+
+# Sincronizza i pacchetti
+pacman -Syy
+
+# Installa i pacchetti base
+pacstrap /mnt base base-devel linux-lts linux-lts-headers linux-firmware nano btrfs-progs ${CPU_MICROCODE}
+
+# Genera fstab
+genfstab -U /mnt >> /mnt/etc/fstab
+
+# Configura il sistema
+mkdir -p /mnt/installa-arch
+cp ./configura.sh /mnt/installa-arch/configura.sh
+arch-chroot /mnt /bin/bash /installa-arch/configura.sh
+rm -rf /mnt/installa-arch
+umount -R /mnt
+
