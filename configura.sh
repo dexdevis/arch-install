fdad22e1a3629f6e367af403ae07ffc2caae6c51
dexdevis <132813755+dexdevis@users.noreply.github.com>
2024-08-30 11:38:19 +0200

    Update configura.sh

--- configura.sh
+++ configura.sh
@@ -219,7 +219,7 @@ mkinitcpio -P
 ##### GRUB
 ################################################
 
-grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB # <------------------------------------------------------
+grub-install --target=x86_64-efi --bootloader-id=grub_uefi --recheck # <------------------------------------------------------
 grub-mkconfig -o /boot/grub/grub.cfg
 
 
