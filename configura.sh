294bd61bd47b4259dd850a6085a045df83e139c3
dexdevis <132813755+dexdevis@users.noreply.github.com>
2024-09-08 14:59:20 +0200

    Update configura.sh

--- configura.sh
+++ configura.sh
@@ -324,7 +324,7 @@ pacman -S --noconfirm timeshift grub-btrfs
 rsync -a /boot /.bootbackup
 
 # Creo il primo snapshot di Backup
-timeshift --create --btrfs --snapshot-device /dev/nvme0n1p2 --comments "Primo Backup" # <-----------------------------------------------------------------
+timeshift --create --btrfs --comments "Primo Backup" # <-----------------------------------------------------------------
 grub-mkconfig -o /boot/grub/grub.cfg
 
 # Per ripristinare un backup: sudo timeshift --restore
